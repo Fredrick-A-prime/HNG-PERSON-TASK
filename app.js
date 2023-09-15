6ddb8f83bfd774f6f9bc4e9113e2f68de62927be
SupremePrimeRick <alfredfredrick41@gmail.com>
2023-09-15 10:01:04 -0500

    modified

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
+// require('dotenv').config();
+const { DB_URL } = require('./props');
 const bodyParser = require('body-parser');
 const PersonRoutes = require('./routes/PersonRoutes');
 
-const DB_URL = process.env.DATABASE_URL;
+// const DB_URL = process.env.DATABASE_URL;
 const app = express();
 mongoose.connect(DB_URL, { useNewUrlParser: true }).then(() => console.log('db connected'));
+/* async function connectToDatabase() {
+    try {
+      await mongoose.connect(DB_URL, { useNewUrlParser: true });
+      console.log('Database connected');
+    } catch (error) {
+      console.error('Error connecting to database:', error.message);
+    }
+}
+connectToDatabase();
+*/
+  
+  
+  
+  
+  
 
 app.use(express());
 app.use(express.json());
---
+++ props.js
@@ -0,0 +1,4 @@
+module.exports = {
+	// DB_PORT: process.env.PORT|4000,
+	DB_URL: 'mongodb+srv://alfredfredrick41:CiZWmqJcA3PnGGk8@hngcluster.kpxmw2t.mongodb.net/?retryWrites=true&w=majority'
+}
