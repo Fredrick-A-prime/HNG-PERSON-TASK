bb5b0196c42582984f0324fc8b35c84563fe7d5e
SupremePrimeRick <alfredfredrick41@gmail.com>
2023-09-16 21:35:43 -0500

    modified

--- controllers/PersonController.js
+++ controllers/PersonController.js
@@ -1,81 +1,62 @@
-const person = require('../models/PersonModel');
+const person = require("../models/PersonModel");
 
-
-/*const createPerson = async (req, res) => {
-    try {
-        const newPerson = await person.create(req.body);
-        res.status(201).json(newPerson);
-
-    } catch (err) {
-        res.status(500).json({ error: 'failed to create person', message: err.message });
-    };
-};*/
 const createPerson = async (req, res) => {
-    try {
-        const { Name } = req.body;
+  try {
+    const { Name } = req.body;
 
-        if (!Name) {
-            return res.status(400).json({ error: 'Name is required' });
-        }
-
-        const newPerson = await person.create({ Name });
-        res.status(201).json(newPerson);
-
-    } catch (err) {
-        res.status(500).json({ error: 'Failed to create person', message: err.message });
+    if (!Name) {
+      return res.status(400).json({ error: "Name is required" });
     }
+
+    const newPerson = await person.create({ Name });
+    res.status(201).json(newPerson);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Failed to create person", message: err.message });
+  }
 };
 
 const getAperson = async (req, res) => {
-    try {
-        const { Name } = req.query;
-        const aPerson = await person.find({Name});
-        if (!aPerson || aPerson.length === 0) {
-            return res.status(404).json({ error: 'No matching data found' });
-        }
-        res.status(200).json(aPerson);
-    } catch (err) {
-        res.status(500).json({ error: 'An error occured' });
-    };
+  try {
+    const { Name } = req.query;
+    const aPerson = await person.find({ Name });
+    if (!aPerson || aPerson.length === 0) {
+      return res.status(404).json({ error: "No matching data found" });
+    }
+    res.status(200).json(aPerson);
+  } catch (err) {
+    res.status(500).json({ error: "An error occured" });
+  }
 };
 const updatePerson = async (req, res) => {
     try {
-        const details = req.body;
-        if (!details || Object.keys(details).length === 0) {
-            return res.status(400).json({ error: 'Details are missing in the request body' });
-        }
-
-        const { query } = req.query;
-        if (!query || Object.keys(query).length === 0) {
-            return res.status(400).json({ error: 'Query parameter is required' });
-        }
-
-        const updatePerson = await person.findOneAndUpdate(query, details, { new: true });
-        if (!updatePerson) throw Error('Can not be updated');
-        res.status(200).json(updatePerson);
+        const query = req.query;
+        const Person = await person.findOneAndUpdate(query,  req.body, { new: true });
+        if (!Person) throw Error('Something went wrong while updating the person');
+        res.status(200).json(`updated sucessfully ${Person}`);
     } catch (err) {
-        res.status(500).json({ error: 'Can not be updated' });
+        res.status(500).json({ error: 'An error occured' });
     }
-};
-
+}
 const deletePerson = async (req, res) => {
-    try {
-        const query = req.query; 
-        if (!query || Object.keys(query).length === 0) {
-            return res.status(400).json({ error: 'Query parameter is required' });
-        }
-
-        const deletePerson = await person.findOneAndDelete(query, {});
-        if (!deletePerson) throw Error('Person not found');
-        res.status(200).json(deletePerson);
-    } catch (err) {
-        res.status(500).json({ error: 'Can not be deleted' });
+  try {
+    const query = req.query;
+    if (!query || Object.keys(query).length === 0) {
+      return res.status(400).json({ error: "Query parameter is required" });
     }
+
+    const deletePerson = await person.findOneAndDelete(query, {});
+    if (!deletePerson) throw Error("Person not found");
+    res.status(200).json(`deleted sucessfully ${deletePerson}`);
+  } catch (err) {
+    res.status(500).json({ error: "Can not be deleted" });
+  }
 };
 
 module.exports = {
-    createPerson,
-    getAperson,
-    updatePerson,
-    deletePerson
-}
+  createPerson,
+  getAperson,
+  updatePerson,
+  deletePerson,
+};
